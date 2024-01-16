40361bd6fc401ba9e4b03f6069c055fadb91a1b9
Thomas <thomasjungcoe@gmail.com>
2024-01-15 22:21:20 -0600

    cleaned up some previous codes and quarterly time frame multile tickers pulling done

--- 09_fa.py
+++ 09_fa.py
@@ -3,9 +3,9 @@ import yfinance as yf
 import os
 
 def download_data(symbol, start_date, end_date, output_dir):
-    data = yf.download(symbol, start=start_date, end=end_date, interval= "1mo")
+    data = yf.download(symbol, start=start_date, end=end_date, interval= "3mo")
     data = data.drop(['Volume'], axis=1)
-    data.to_csv(os.path.join(output_dir, f'US-{symbol}-M.csv'))
+    data.to_csv(os.path.join(output_dir, f'US-{symbol}-Q.csv'))
 
 def main():
     parser = argparse.ArgumentParser()
